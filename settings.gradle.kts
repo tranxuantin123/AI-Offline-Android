e340e7f97925057bb8fc2f8c14009f940fde47ea
tranxuantin123 <xuanxuantin123@gmail.com>
2025-10-21 20:39:54 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name = "AI-Offline"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AI-Offline-Android"
 include(":app")
